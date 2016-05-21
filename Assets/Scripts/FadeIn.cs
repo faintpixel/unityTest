56b710dcd52ef605354fd9fbf6bcd14d46439653
faintpixel <artomizer@sketchdaily.net>
2016-05-21 09:08:40 -0600

    added a fade in and some camera tweaks

---
+++ Assets/Scripts/FadeIn.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeIn : MonoBehaviour {
+    public float Speed;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(PerformFade());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private IEnumerator PerformFade()
+    {
+        MeshRenderer renderer = GetComponent<MeshRenderer>();
+        Material material = renderer.material;// GetComponent<Material>();
+
+        for(float i = 0; i <= 1; i += Speed)
+        {
+            Debug.Log("Set transparency to " + i);
+            material.color = new Color(material.color.r, material.color.g, material.color.b, i);
+            yield return new WaitForSeconds(Speed);
+        }
+
+        material.color = new Color(material.color.r, material.color.g, material.color.b, 1f);
+
+        Destroy(this);
+    }
+}
---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLook : MonoBehaviour {
+    public float MoveSpeed = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h = MoveSpeed * Input.GetAxis("Mouse Y") * -1;
+        float v = MoveSpeed * Input.GetAxis("Mouse X");
+
+        transform.Rotate(h, v, 0);
+    }
+}
